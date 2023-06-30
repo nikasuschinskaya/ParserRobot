26fb39e1dd93b6d9819e4d232ec9f12473323bf4
nikasuschinskaya@gmail.com <nikasuschinskaya@gmail.com>
2023-06-30 18:22:32 +0300

    some corrects

--- ParserRobot/ParserRobot.UI/App.xaml.cs
+++ ParserRobot/ParserRobot.UI/App.xaml.cs
@@ -1,18 +1,9 @@
-﻿using Autofac;
-using ParserRobot.UI.Container;
-using System.Windows;
+﻿using System.Windows;
 
 namespace ParserRobot.UI
 {
     public partial class App : Application
     {
-        public static IContainer Container { get; private set; }
-
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-
-            Container = ContainerConfig.Configure();
-        }
+        protected override void OnStartup(StartupEventArgs e) => base.OnStartup(e);
     }
 }
